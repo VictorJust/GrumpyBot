6950e07940c18fd44f4472f13050bface84afa95
VictorJust <victorjust09@gmail.com>
2023-06-14 13:05:01 +0300

    Add buttons to the bot menu

--- TelegramBotTest/Program.cs
+++ TelegramBotTest/Program.cs
@@ -6,6 +6,7 @@ using Telegram.Bots.Extensions.Polling;
 using Telegram.Bot.Types;
 using Telegram.Bot.Exceptions;
 using Telegram.Bot.Polling;
+using Telegram.Bot.Types.ReplyMarkups;
 
 namespace TelegramBotExperiments
 {
@@ -17,29 +18,57 @@ namespace TelegramBotExperiments
         {
             // Wait for a text from a user and/or send greetings message
             Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(update));
+
             if (update.Type == Telegram.Bot.Types.Enums.UpdateType.Message)
             {
                 var message = update.Message;
 
-                switch (message.Text.ToLower())
+                if (message.Text == "/start")
+                {
+                    // Create an instance of InlineKeyboardMarkup
+                    var inlineKeyboard = new InlineKeyboardMarkup(new[]
+                    {
+                new[]
+                {
+                    InlineKeyboardButton.WithCallbackData("Start", "/start"),
+                    InlineKeyboardButton.WithCallbackData("Author info", "/authorinfo"),
+                },
+                new[]
+                {
+                    InlineKeyboardButton.WithCallbackData("What a bot", "/whatabot"),
+                    InlineKeyboardButton.WithCallbackData("Call the manager", "option4"),
+                }
+            });
+
+                    // Send the inline keyboard as a reply to the user's message
+                    await botClient.SendTextMessageAsync(message.Chat, "Please choose an option:", replyMarkup: inlineKeyboard);
+                }
+            }
+            else if (update.Type == Telegram.Bot.Types.Enums.UpdateType.CallbackQuery)
+            {
+                var callbackQuery = update.CallbackQuery;
+                var data = callbackQuery.Data;
+
+                // Handle the callback query based on the data received
+                switch (data)
                 {
                     case "/start":
-                        await botClient.SendTextMessageAsync(message.Chat, " Good day, or bad one. Don't care, actually." +
+                        await botClient.SendTextMessageAsync(callbackQuery.Message.Chat, " Good day, or bad one. Don't care, actually." +
                             "\n /authorinfo - guess what it is about" +
                             "\n /whatabot - why you are here, why I am here");
                         break;
                     case "/authorinfo":
-                        await botClient.SendTextMessageAsync(message.Chat, " Why not, who wants privacy? Here we go. " +
+                        await botClient.SendTextMessageAsync(callbackQuery.Message.Chat, " Why not, who wants privacy? Here we go. " +
                             "\n HH: https://hh.ru/resume/cc62699eff0bc63f340039ed1f63564c593858" +
                             "\n LinkedIn: https://www.linkedin.com/in/victor-evseenko-99218b25b/" +
                             "\n GitHub: https://github.com/VictorJust");
                         break;
                     case "/whatabot":
-                        await botClient.SendTextMessageAsync(message.Chat, " In previous life I was cursed for... None of you business actually. " +
+                        await botClient.SendTextMessageAsync(callbackQuery.Message.Chat, " In previous life I was cursed for... None of you business actually. " +
                             "\n And now I am a bot manager. So ask me anything. I won't be surprised, won't care and most likely won't be helpful.");
                         break;
                     default:
-                        await botClient.SendTextMessageAsync(message.Chat, "I wasn't waiting for anyone. But whatever, feel yourself at home.");
+                        await botClient.SendTextMessageAsync(callbackQuery.Message.Chat, "I wasn't waiting for anyone. But whatever, feel yourself at home.");
                         break;
                 }
             }
